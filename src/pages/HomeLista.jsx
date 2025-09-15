33b76c935e3d1a1c5a5769d31e3931835db975cd
NikRedFox <nikolasbs11@gmail.com>
2025-09-15 16:39:23 -0300

    Feat: Finished HomeLista.jsx

--- src/pages/HomeLista.jsx
+++ src/pages/HomeLista.jsx
@@ -1,8 +1,9 @@
-import { Dimensions, Image, TouchableOpacity, View, Text } from 'react-native';
+import { Dimensions, Image, TouchableOpacity, View, Text, FlatList } from 'react-native';
 import {useEffect, useState} from 'react';
 import styled from 'styled-components/native';
 import backgroundImage from '../assets/mobileBg.png'
 import split from '../assets/buttonSplit.png';
+import divisoria from '../assets/divisoriaLista.png';
 import Button from '../components/Button.jsx'
 import * as Font from 'expo-font';
 
@@ -16,7 +17,7 @@ const ListaContainer = styled.ImageBackground`
 const Texto = styled.Text`
   color: #E3B779;
   font-size: 60px;
-  padding: 55px 0px 65px 0px;
+  padding: 55px 0px 6px 0px;
   text-align: center;
   font-family: 'ParkLaneNF';
 `
@@ -24,20 +25,59 @@ const Texto = styled.Text`
 const Split = styled.Image`
 `
 
-const ButtonWrapper = styled.View`
-    gap: 28px;
-    display: flex;
-    flex-direction: row;
+const Divisoria = styled.Image`
+  width: 90%;
+  display: flex;
+  align-self: center;
 `
 
-// const BglessButton = styled.TouchableOpacity`
-//   width: 100px;
-//   height: 50px;
-//   background-color: transparent;
-// `
+const Card = styled.View`
+  padding: 16px;
+  margin: 8px 16px;
+  border-radius: 12px;
+  /* border: 1px solid #292725; */
+  gap: 10px;
+`
+
+const Placa = styled.Text`
+  color: #E3B779;
+  font-size: 25px;
+  font-family: 'Milonga';
+`
+
+const Info = styled.Text`
+  color: #E3B779;
+  font-size: 20px;
+  font-family: 'Milonga';
+`
+
+const ButtonContainer = styled.View`
+  height: 180px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  gap: 10px;
+`
+
+const BglessButtonE = styled.Text`
+  font-family: 'Milonga';
+  color: #E3B779;
+  font-size: 30px;
+`
+
+const BglessButtonS = styled.Text`
+  font-family: 'Milonga';
+  color: #E3B779;
+  font-size: 30px;
+  width: 105px;
+  text-align: center;
+`
 
 export default function HomeLista(){
-    const [loaded, setLoaded] = useState(false);
+    const [loaded, setLoaded] = useState(false)
+    const [carros, setCarros] = useState([]);
 
     useEffect(() => {
       Font.loadAsync({
@@ -46,17 +86,61 @@ export default function HomeLista(){
       }).then(() => setLoaded(true));
     }, []);
 
+
+    useEffect(()=>{
+      const dadosFake = [
+        {id: '1', placa: 'ABC-1234', data: '15/09/2025', hora: '14:30'},
+        {id: '2', placa: 'DEF-5678', data: '15/09/2025', hora: '14:30'},
+        {id: '3', placa: 'GHI-9101', data: '15/09/2025', hora: '14:30'},
+        {id: '4', placa: 'JKL-1121', data: '15/09/2025', hora: '14:30'},
+        {id: '5', placa: 'MNO-3141', data: '15/09/2025', hora: '14:30'},
+        {id: '6', placa: 'PQR-5161', data: '15/09/2025', hora: '14:30'},
+      ];
+
+      setCarros(dadosFake);
+    }, [])
+    
     if (!loaded) return null;
+
+    const renderItem = ({item}) =>(
+      <Card>
+        <Placa>Placa:    {item.placa}</Placa>
+        <Info>Entrada:    {item.data}</Info>
+        <Info>Hora:         {item.hora}</Info>
+      </Card>
+    )
   
     return(
          <ListaContainer source ={backgroundImage} resizeMode='cover'>
-                  <Texto>Estacionamento Golden Flex</Texto>
-                  
-                  
-                  <Button texto="Entrar" /*onPress={alert("Funcionou")}*//>
-                  <Button texto="Cadastro"/>
-                    
-                </ListaContainer>
+            <Texto>Lista</Texto>             
+            <View style={{flex:1}}>
+              <FlatList
+                data={carros}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                ItemSeparatorComponent={()=> (
+                  <Divisoria
+                    source={divisoria}
+                  />
+                )}
+              /> 
+            </View>
+
+            <ButtonContainer>
+              <TouchableOpacity>
+                <BglessButtonE>Entrada</BglessButtonE>
+              </TouchableOpacity>
+
+              <Split source={split}></Split>
+
+              <TouchableOpacity>
+                <BglessButtonS>Saida</BglessButtonS>
+              </TouchableOpacity>
+            </ButtonContainer>
+
+                   
+          </ListaContainer>
+
     )
 };
 
